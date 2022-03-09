58b6a40928a4f2cc6741aba6cb59b4606e138bb4
AshtonHarvey <33674402+AshtonHarvey@users.noreply.github.com>
2022-03-08 20:49:33 -0500

    initial set up
    added project files. set up git ignore. set up parcel bundler

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import "./styles.css";
+import "./scss/styles.scss";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
+  info about Parcel. Using this to test and set up the base project.
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
