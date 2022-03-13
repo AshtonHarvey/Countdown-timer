5c2613c0f67f6b5ed2ee0926151ae73d811d0208
AshtonHarvey <33674402+AshtonHarvey@users.noreply.github.com>
2022-03-12 20:54:25 -0500

    date input now functioning
    finished implementing a basic date input  for the user to create their own count down.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ const counterHours = document.getElementById("hours");
 const counterMinutes = document.getElementById("minutes");
 const counterSeconds = document.getElementById("seconds");
 const timerDirection = document.getElementById("timer-direction");
+const inputDate = document.getElementById("input-date");
+const inputTime = document.getElementById("input-time");
+
+let countDownDate = new Date("march 9, 2022 19:00:00").getTime();
+
+inputDate.addEventListener("change", () => {
+  updateTimeDate();
+});
+inputTime.addEventListener("change", () => {
+  updateTimeDate();
+});
+
+function updateTimeDate() {
+  //take in date and time inpout and create/ update the date use for count down
+  //console.log("change date is working");
+  if (inputDate == null || inputTime == null) {
+    console.log("missing date or time");
+  } else {
+    countDownDate = new Date(`${inputDate.value} ${inputTime.value}`);
+  }
+}
 
 //store time till date as milliseconds
-let countDownDate = new Date("march 9, 2022 19:00:00").getTime();
 
 let t = setInterval(() => {
   //store current date and time
@@ -23,12 +43,12 @@ let t = setInterval(() => {
   //check if count down is finished
   //use < for extra second to see clock at all 0. otherwise use <=
   if (distance < 0) {
-    //use clearinterval(t) to stop the count.
-    //to not stop dont clear and * by -1 to keep time past point
-    days *= -1;
-    hours *= -1;
-    minutes *= -1;
-    seconds *= -1;
+    //use to stop the count.
+    //clearinterval(t);
+    days = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
   }
 
   //Display results in place holder element
@@ -36,5 +56,4 @@ let t = setInterval(() => {
   counterHours.innerHTML = `${hours}`;
   counterMinutes.innerHTML = `${minutes}`;
   counterSeconds.innerHTML = `${seconds}`;
-  console.log(`${days}`);
 }, 1000);
